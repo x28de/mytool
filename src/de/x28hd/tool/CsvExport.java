47bff810503a145f18c8af91ee6700f674bc5007
x28de <matthiasmelcher@web.de>
2017-01-28 19:08:19 +0100

    CSV Export improved

--- src/de/x28hd/tool/CsvExport.java
+++ src/de/x28hd/tool/CsvExport.java
@@ -2,12 +2,21 @@ package de.x28hd.tool;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Enumeration;
 import java.util.Hashtable;
+
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLEditorKit;
 
 public class CsvExport {
 	Hashtable<Integer,GraphNode> nodes;
 	Hashtable<Integer,GraphEdge> edges;
+	String htmlOut;
+	String breaks;
+	boolean start;
 	
 	public CsvExport(Hashtable<Integer,GraphNode> nodes, Hashtable<Integer,GraphEdge> edges, 
 		String storeFilename, GraphPanelControler controler) {
@@ -23,12 +32,55 @@ public class CsvExport {
 				label = label.replace("\n", "");
 				String detail = node.getDetail();
 				detail = detail.replace("\n", "");
+				detail = filterHTML(detail);
 				list.write(label + "\t" + detail + newLine);
 			}
-			list.write("done");
 			list.close();
 		} catch (IOException e) {
 			System.out.println("Error CSV101 " + e);			
+		}
+	}
+	private String filterHTML(String html) {
+		htmlOut = "";
+		breaks = "";
+		start = true;
+		MyHTMLEditorKit htmlKit = new MyHTMLEditorKit();
+		HTMLEditorKit.Parser parser = null;
+		HTMLEditorKit.ParserCallback cb = new HTMLEditorKit.ParserCallback() {
+			public void handleText(char[] data, int pos) {
+				String dataString = new String(data);
+				htmlOut = htmlOut + breaks + dataString;
+				start = false;
+				breaks = "";
+			}
+			public void handleSimpleTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+				if (t.equals(HTML.Tag.BR)) {
+					if (!start) breaks = breaks + "<br />";
+					return;
+				}
+			}
+		};
+		parser = htmlKit.getParser();
+		Reader reader; 
+		reader = (Reader) new StringReader(html);
+		try {
+			parser.parse(reader, cb, true);
+		} catch (IOException e2) {
+			System.out.println("Error CSV109 " + e2);
+		}
+		try {
+			reader.close();
+		} catch (IOException e3) {
+			System.out.println("Error CSV110 " + e3.toString());
+		}
+		return htmlOut;
+	}
+
+	private static class MyHTMLEditorKit extends HTMLEditorKit {
+		private static final long serialVersionUID = 7279700400657879527L;
+
+		public Parser getParser() {
+			return super.getParser();
 		}
 	}
 }
