315e77d7a83fd9d5cb0f5f87bbeb8bcaac8ad5f6
x28de <matthiasmelcher@web.de>
2018-01-06 21:44:01 +0100

    bundles raus aber Icons zZ doppelt :-(
    Tut bis auf graue Shortcuts

--- src/de/x28hd/tool/AboutBuild.java
+++ src/de/x28hd/tool/AboutBuild.java
@@ -9,7 +9,7 @@ public class AboutBuild {
 	
 	about =  " ******** Provisional BANNER ********* " +
 			"\r\n " + 
-			"\r\n This is Condensr Release 45 Build 2 " + extIndicator +
+			"\r\n This is Condensr Release 45 Build 3 " + extIndicator +
 			"\r\n running on Java version " + System.getProperty("java.version") +
 			"\r\n on " + System.getProperty("os.name") + " version " + System.getProperty("os.version") +
 			" (os.arch = " + System.getProperty("os.arch") + ")" +
