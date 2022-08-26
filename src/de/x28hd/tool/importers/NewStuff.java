56a5a492db8830483acc6d655e2b46577d1a3be7
Matthias Melcher <matthias.melcher@t-online.de>
2022-08-26 18:22:47 +0200

    Cleanup

--- src/de/x28hd/tool/importers/NewStuff.java
+++ src/de/x28hd/tool/importers/NewStuff.java
@@ -1,7 +1,6 @@
 package de.x28hd.tool.importers;
 
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
@@ -9,43 +8,21 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Hashtable;
 import java.util.List;
 
 import javax.swing.TransferHandler;
 
 import de.x28hd.tool.PresentationService;
 import de.x28hd.tool.PresentationExtras;
-import de.x28hd.tool.Utilities;
-import de.x28hd.tool.core.GraphEdge;
-import de.x28hd.tool.core.GraphNode;
 
 public class NewStuff {
-	
-	
-	//	Main fields
-	Hashtable<Integer, GraphNode> newNodes = new Hashtable<Integer, GraphNode>();
-	Hashtable<Integer, GraphEdge> newEdges = new Hashtable<Integer, GraphEdge>();
-	Point dropLocation = null;
-	
-	//	About caller (PresentationService()) and CompositionWindow()
 	private PresentationService controler;
 	PresentationExtras controlerExtras;
+	Point dropLocation = null;
 	boolean compositionMode = false;
-	
-	//	Input
-	public static DataFlavor htmlSelectionFlavor;
 	String advisableFilename = "";
-	
-	//	Map loading
-	boolean existingMap = false;
-	Rectangle bounds = new Rectangle(2, 2, 2, 2);
-	
-	//	HTML exploitation
 	boolean parseMode = false; 
 	boolean dropEncoding = true;
-	
 	String dataStringResort = "";
 
 	
@@ -62,37 +39,25 @@ public class NewStuff {
 	
 	public boolean canImport(TransferHandler.TransferSupport support, String diag) {
 		support.setDropAction(TransferHandler.COPY);
-//        support.setShowDropLocation(true);    // no effect ?
         if (!support.isDataFlavorSupported(DataFlavor.javaFileListFlavor) && 
 			!support.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-//			System.out.println("NS dataflavors: " + support.getDataFlavors() + " " + support.getDataFlavors().length);
-			DataFlavor[] df = support.getDataFlavors();
-			for (int i = 0; i < df.length; i++) {
-				System.out.println("NS dataflavors: " + df[i].getHumanPresentableName());
-			}
         	return false;
         }
 //		System.out.println("NS canImport via " + diag);
-		return true;
+        return true;
 	}
 	
 	public boolean importData(TransferHandler.TransferSupport support, String diag) {
-   	 
-        if (!canImport(support, diag)) {
-           return false;
-        }
-
+		if (!canImport(support, diag)) {
+			return false;
+		}
         Transferable t = support.getTransferable();
 		dropLocation = new Point(support.getDropLocation().getDropPoint().x,
 				support.getDropLocation().getDropPoint().y);
-//		System.out.println("NS Drop point " + support.getDropLocation().getDropPoint().x + ", " +
-//				support.getDropLocation().getDropPoint().y);
-       
-        boolean success = transferTransferable(t);
-		if (!success) {
-			System.out.println("Error NS120.");
-		}
-        return success;
+		boolean success = 
+				transferTransferable(t);
+		if (!success) System.out.println("Error NS120.");
+		return success;
 	}
 
 	public Transferable readClipboard() {
@@ -125,7 +90,6 @@ public class NewStuff {
 	@SuppressWarnings("unchecked")
 	public boolean transferTransferable(Transferable content) {
 		String dataString = "";			
-		Utilities utilities = new Utilities();
 		
 		//  File(s) ?
 
@@ -171,38 +135,9 @@ public class NewStuff {
 				System.out.println("Error NS113a " + e1);
 				return false;
 			}
-//			System.out.println("NS: was fragmentHtmlFlavor: \r\n");
+			System.out.println("NS: was fragmentHtmlFlavor: \r\n");
 			new Step2b(dataString, controler, true);
 			return true;
-		} else {
-		try {
-			htmlSelectionFlavor = new DataFlavor("text/html;charset=utf-8;class=java.io.InputStream");
-			// "unicode" was wrong: yielded space after each character, 
-			// and HTMLEditorKit.ParserCallback() recognized nothing but body p-implied
-			
-			// Update June 2016: still works from Eclipse but not from Jar. Trying to
-			// keep it until advice may come along
-		} catch (ClassNotFoundException cle) {
-			System.out.println("Error NS111 " + cle);
-			return false;
-		}
-		if (content.isDataFlavorSupported(htmlSelectionFlavor)) {
-			try {
-				InputStream in = (InputStream) content.getTransferData(htmlSelectionFlavor);
-				dataString = utilities.convertStreamToString(in);
-				in.close();
-				dataStringResort = (String) content.getTransferData(DataFlavor.stringFlavor);
-			} catch (UnsupportedFlavorException e1) {
-				System.out.println("Error NS112 " + e1);
-				return false;
-			} catch (IOException e1) {
-				System.out.println("Error NS113 " + e1);
-				return false;
-			}
-//			System.out.println("NS: was htmlSelectionFlavor: \r\n");
-			new Step2b(dataString, controler, true);
-			return true;
-		} 
 		}
 
 		//	Plain string ?
@@ -236,16 +171,16 @@ public class NewStuff {
     
 	public void setInput(String dataString, int inputType) {
 		// TODO: fit to just 1, 2 and few 6
-		if (inputType == 1) {
+		if (inputType == 1) {	// e.g. from start parmameter
 
 			controler.getNSInstance().setAdvisableFilename(dataString);
 			new InterceptZips(new File(dataString), controler);
 		}
-		else if (inputType == 2) {
+		else if (inputType == 2) {	// e.g. from importers or copied map fragments
 
 			new AnalyzeBlob(dataString, controler);
 		}
-		else {
+		else {	// e.g. for CompositionWindow, inputType 6
 
 			new Step2b(dataString, controler, false);
 		}
@@ -259,6 +194,10 @@ public class NewStuff {
 	
 	public void setParseMode(boolean toggle) {
 		this.parseMode = toggle;
+	}
+	
+	public String getDataStringResort() {
+		return dataStringResort;
 	}
 	
 	public void setDropEncoding(boolean toggle) {
