1d6015ad0664878d2042aeb74d5cf251503ea4d9
Matthias Melcher <matthias.melcher@t-online.de>
2024-07-09 17:22:20 +0200

    Nested lists input

--- src/de/x28hd/tool/inputs/IngestItemlists.java
+++ src/de/x28hd/tool/inputs/IngestItemlists.java
@@ -1,11 +1,14 @@
 package de.x28hd.tool.inputs;
 
+import java.awt.Color;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.Hashtable;
 
+import javax.swing.JOptionPane;
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
@@ -22,22 +25,63 @@ public class IngestItemlists {
 	PresentationService controler;
 	Rectangle bounds = new Rectangle(2, 2, 2, 2);
 	String htmlOut = "";
+	boolean htmlNoise = false;
+	
+	// for filterHTML1
 	boolean listItem = false;
 	boolean silentlyResort = false;
 	boolean belowHeading = false;
-	boolean htmlNoise = false;
 	boolean structureFound = false;
 	boolean listStructure = false;
+	
+	// for filterHTML2
+	Hashtable<Integer,Integer> olItem = new Hashtable<Integer,Integer>();
+	int item = 0;
+	Hashtable<Integer,Integer> olParents = new Hashtable<Integer,Integer>();
+	int level = 0;
+	Hashtable<Integer,Integer> nodeParents = new Hashtable<Integer,Integer>();
+	int seq = 0;
+	String indent = "";		// to format testing output, then to detect a peculiar error
+	boolean recordEmpty = true;
+	boolean heading = false;
+	boolean ol = false;
+	
+	// Usual auxiliary fields
+	int j = 0;
+	int maxVert = 10;
+	int edgesNum = 0;
+	String [] edgePalette = 	// Copied from Gui()
+		{"#d2bbd2", "#bbbbff", "#bbffbb", "#ffff99", "#ffe8aa", "#ffbbbb"};
 	
 	public IngestItemlists(String dataString, PresentationService controler, 
 			boolean parseHtml) {
 		this.controler = controler;
 		boolean compositionMode = controler.getNSInstance().compositionMode;
 
-    	if (parseHtml) dataString = filterHTML(dataString);
+		// New: tree, uses filterHTML2 
+		if (parseHtml) {
+			if (dataString.toLowerCase().contains("<ol") 
+					|| dataString.toLowerCase().contains("<ul") 
+					|| dataString.toLowerCase().contains("<dl")) {
+				Object[] options =  {"No", "Yes"};
+				int	response = JOptionPane.showOptionDialog(null,
+						"Hierarchical arrangement wanted?\n",
+						"Option", JOptionPane.YES_NO_OPTION, 
+						JOptionPane.QUESTION_MESSAGE, null, 
+						options, options[1]);
+				if (response == 1) {
+					handleTrees(dataString);
+					new InsertMap(controler, newNodes, newEdges, bounds, false);
+					return;
+				}
+			}
+		}
+		
+		// Old, uses filterHTML1, crude as before
+		if (parseHtml) dataString = filterHTML1(dataString);
 		if (compositionMode) {
-	    	controler.getControlerExtras().getCWInstance().insertSnippet(dataString);
-	    	return;
+		controler.getControlerExtras().getCWInstance().insertSnippet(dataString);
+		return;
 		} 
 		SplitIntoNew splitIntoNew = new SplitIntoNew(controler);
 //		int newNodesCount = splitIntoNew.separateRecords(dataString);
@@ -50,10 +94,81 @@ public class IngestItemlists {
 		new InsertMap(controler, newNodes, newEdges, bounds, false);
 	}
 	
-//
-//	Stuff from HTML lists
+	public void handleTrees(String dataString) {
+		String dataString2 = filterHTML2(dataString);
+		dataString = dataString2;
+		String [] lines = dataString.split("\\r?\\n");
+		int numRec = lines.length;
+		for (int i = 0; i < numRec; i++) {
+			String line = lines[i];
+			int id = i + 1;
+			if (!line.contains("\t")) {
+				System.out.println("No level: " + line);
+				line = "0\t" + line;
+			}
+			String[] fields = line.split("\\t");
+			int level = Integer.parseInt(fields[0]);
+			String record = fields[1];
+			addNode(record, level);		// should get the above id as its key
+			if (nodeParents.containsKey(id)) {
+				int parentID = nodeParents.get(id);
+				// Diagnostics mitigates that parser is not thread-safe?
+				if (!newNodes.containsKey(id)) {
+					System.out.println("No child " + id);
+					continue;
+				}
+				if (!newNodes.containsKey(parentID)) {
+					System.out.println("No parent " + parentID);
+					continue;
+				}
+				edgesNum++;
+				GraphNode node1 = newNodes.get(id);
+				GraphNode node2 = newNodes.get(parentID);
+				String colorString = edgePalette[level % 6];
+
+				GraphEdge edge = new GraphEdge(edgesNum, node1, node2, Color.decode(colorString), "");
+				newEdges.put(edgesNum, edge);
+			} else {
+				System.out.println(id + " has no parent");
+			}
+		}
+	}
 	
-	private String filterHTML(String html) {
+	public GraphNode addNode(String record, int level) {
+		String label = "";
+		j++;
+		int id = j;
+
+		int y = 40 + j * 40;
+		int x = 40 + level * 150;
+		Point p = new Point(x, y);
+		int offset = record.indexOf("<br");
+		if (offset > 0 && offset < 30) {
+			label = record.substring(0, offset);
+		} else if (record.length() < 30) {
+			label = record;
+		} else {
+			offset = record.indexOf("(");
+			if (offset > 0 && offset < 30) {
+				label = record.substring(0, offset);
+			} else {
+				offset = record.indexOf(",");
+				if (offset > 0 && offset < 30) {
+					label = record.substring(0, offset);
+				} else {
+					label = record.substring(0, 30);
+				}
+			}
+		}
+		GraphNode topic = new GraphNode (id, p, Color.decode("#ccdddd"), label, record);	
+		newNodes.put(id, topic);
+		return topic;
+	}
+	
+//
+//	Stuff from HTML 
+	
+	private String filterHTML1(String html) {
 //		System.out.println(html);
 
 		htmlOut = "";
@@ -110,7 +225,6 @@ public class IngestItemlists {
 				}
 			}
 			public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
-//				Enumeration<?> attrs = a.getAttributeNames();
 				if (t == HTML.Tag.PRE) {
 					silentlyResort = true;
 				} else if (t.toString() == "li") {
@@ -129,6 +243,7 @@ public class IngestItemlists {
 					if (!structureFound && htmlOut != "") htmlOut = "\t" + htmlOut;
 					htmlOut = htmlOut + "\n";
 				}
+				if (t == HTML.Tag.P) htmlOut += "<p>";
 			}
 			public void handleSimpleTag(HTML.Tag t, MutableAttributeSet a, int pos) {
 				if (t == HTML.Tag.BR && (belowHeading || !structureFound)) {
@@ -142,12 +257,12 @@ public class IngestItemlists {
 		try {
 			parser.parse(reader, cb, true);
 		} catch (IOException e2) {
-			System.out.println("Error NS128 " + e2);
+			System.out.println("Error IIL128a " + e2);
 		}
 		try {
 			reader.close();
 		} catch (IOException e3) {
-			System.out.println("Error NS129 " + e3.toString());
+			System.out.println("Error IIL129a " + e3.toString());
 		}
 		if (!structureFound || silentlyResort) {
 			if (!silentlyResort) {
@@ -158,5 +273,105 @@ public class IngestItemlists {
 		}
 		return htmlOut;
 	}
+		
+	private String filterHTML2(String html) {
+		htmlOut = "";
+		htmlNoise = false;
+		if (System.getProperty("os.name").startsWith("Windows")) htmlNoise = true;
+		if (html.startsWith("Version:")) htmlNoise = true;
+		
+		// We copy each line, preceding by its level and a tab character,
+		// Also, we record its sequence number seq to store the nodeParent relationship for
+		// handleTree() to draw the connector lines.
+		// And we store olItem and olParent per level to reconstruct the OL item numbers.
 
+		MyHTMLEditorKit htmlKit = new MyHTMLEditorKit();
+		HTMLEditorKit.Parser parser = null;
+
+		HTMLEditorKit.ParserCallback cb = new HTMLEditorKit.ParserCallback() {
+			public void handleEndTag(HTML.Tag t, int pos) {
+				if (t == HTML.Tag.OL || t == HTML.Tag.UL || t == HTML.Tag.DL) {
+					if (indent.length() < 2) {		// unexplainable excessive end tag encountered
+						controler.displayPopup("Parsing failed; sorry.\n" +
+							"For once, try without any lines that aren't list items.");
+					}
+					indent = indent.substring(2);
+					level--;
+				} else if (t == HTML.Tag.LI || t == HTML.Tag.DD) {
+					if (!recordEmpty) {
+						htmlOut = htmlOut + "\r\n";
+						seq++;
+						recordEmpty = true;
+						int seqParent = olParents.get(level);
+						nodeParents.put(seq, seqParent);
+					}
+				} else if (t.toString().matches("h\\d")) {
+					heading = false;
+						htmlOut = htmlOut + "</" + t.toString() + ">\r\n";
+						seq++;
+				}
+			}
+			public void handleText(char[] data, int pos) {
+				int len = data.length;
+				if (len <= 0) return;
+				String dataString2 = "";
+				if (len > 0) dataString2 = new String(data);
+				if (htmlNoise) return;
+				if (heading) {
+					heading = false;
+					htmlOut += dataString2;
+					return;
+				}
+				htmlOut = htmlOut + dataString2;
+				recordEmpty = false;
+			}
+			public void handleComment(char[] data, int pos) {
+				String dataString2 = new String(data);
+				if (dataString2.contains("StartFragment")) {
+					htmlNoise = false;
+				}
+			}
+			public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+				if (htmlNoise) return;
+				if (t == HTML.Tag.OL || t == HTML.Tag.UL || t == HTML.Tag.DL) {
+					ol = (t == HTML.Tag.OL);
+					if (!recordEmpty) {
+						htmlOut = htmlOut + "\r\n";
+						seq++;
+						recordEmpty = true;
+						int seqParent = olParents.get(level);
+						nodeParents.put(seq, seqParent);
+					}
+					indent += "  ";
+					level++;
+					olItem.put(level, 0);
+					olParents.put(level, seq);
+				} else if (t == HTML.Tag.LI || t == HTML.Tag.DD) {
+					item = olItem.get(level);
+					item++;
+					olItem.put(level, item);
+					String itemString = ol ? item + ". " : "";
+					htmlOut = htmlOut + level + "\t" + itemString;
+				} else if (t.toString().matches("h\\d")) {
+					heading = true;
+					htmlOut = htmlOut + level + "\t<" + t.toString() + ">";
+				}
+			}
+			public void handleSimpleTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+				if (t == HTML.Tag.BR) {
+					htmlOut = htmlOut + "<br />";
+				}
+			}
+		};
+		parser = htmlKit.getParser();
+		StringReader reader; 
+		reader = new StringReader(html);
+		try {
+			parser.parse(reader, cb, true);
+		} catch (IOException e2) {
+			System.out.println("Error IIL128b " + e2);
+		}
+		reader.close();
+		return htmlOut;
+	}
 }
