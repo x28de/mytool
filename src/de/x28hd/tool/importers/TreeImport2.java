02216b750fc3ac77759f314a2b29a0813f0f04af
Matthias Melcher <matthiasmelcher@web.de>
2026-05-02 20:02:26 +0200

    Mess detection function
    in TreeImport2 (file trees)

--- src/de/x28hd/tool/importers/TreeImport2.java
+++ src/de/x28hd/tool/importers/TreeImport2.java
@@ -23,15 +23,21 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeSelectionModel;
 import javax.xml.transform.TransformerConfigurationException;
 
 import org.stackoverflowusers.file.WindowsShortcut;
@@ -71,6 +77,13 @@ public class TreeImport2 implements ActionListener {
 	Hashtable<Integer,String> edgeColors = new Hashtable<Integer,String>();
 	String[] legend = new String[6];
 	
+	// for mess
+	TreeMap<Double,Integer> messMap = new TreeMap<Double,Integer>();
+	SortedMap<Double,Integer> messList = (SortedMap<Double,Integer>) messMap;
+	Double disambig = .0001;
+
+	JTree tree;
+	DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
 	JDialog frame;
 	JFrame progressFrame;
 	File file;
@@ -90,6 +103,8 @@ public class TreeImport2 implements ActionListener {
 	JCheckBox layoutBox = null;
 	boolean colorOpt = false;
 	JCheckBox colorBox = null;
+	boolean messOpt = false;
+	JCheckBox messBox = null;
 	boolean legendOpt = false;
 	JCheckBox legendBox = null;
 	boolean hypOpt = true;
@@ -98,6 +113,7 @@ public class TreeImport2 implements ActionListener {
 	boolean windows = false;
 	boolean silent = false;
 	boolean silent2 = false;	// no progress bar
+	boolean cancelled = false;
 	int monitor = 0;
 	int myProgress = 0;
 	
@@ -147,14 +163,12 @@ public class TreeImport2 implements ActionListener {
 		// Load stuff
         
 		windows = (System.getProperty("os.name").startsWith("Windows"));
-		colorOpt = true;
 		fs = System.getProperty("file.separator");
-		topNode = file.getAbsolutePath();
-		topNode = createRelatedNode(topNode, false);
+		topNode = file.getName();
+		topNode = createRelatedNode(topNode);
 		int topNum = inputID2num.get(topNode);
 		top = new DefaultMutableTreeNode(new BranchInfo(topNum, file.getName()));
 		myProgress = 5;
-		SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
 
 		// Drill down
 		fileTree(file, topNode, top, 0);
@@ -164,47 +178,6 @@ public class TreeImport2 implements ActionListener {
 				"</a>" + "<br />" + content2 + "<br /></body></html>";
 		GraphNode node = nodes.get(topNum);
 		node.setDetail(moreDetail);
-
-		// Prepare coloring the folders by age 
-		// (by hierarchy is done via addEdge) 			TODO make more transparent
-
-		SortedSet<Long> datesSet = (SortedSet<Long>) datesList.keySet();
-		Iterator<Long> ixit = datesSet.iterator(); 
-		if (datesSet.size() > 0) {
-			int rangeSize = (datesSet.size() / 6) + 1;
-			int counter = datesSet.size() - 1;
-			int previousCol = -1;
-
-			while (ixit.hasNext()) {
-				int colID = counter / rangeSize;
-				counter--;
-				Long modDate = ixit.next();
-				int nodeNum = datesList.get(modDate);
-
-				nodeColors.put(nodeNum, colors[colID]);
-
-				//	Legend item
-				Date date = new Date(modDate);
-				String dateText = df2.format(date);
-				if (colID != previousCol) {
-					String label = dateText + " +";
-					if (colID == 0) label = label + " (newest)";
-					if (colID == 5) label = label + " (oldest)";
-					legend[colID] = label;
-					previousCol = colID;
-				}
-			}
-		}
-		
-		//	Collect relationships
-		Enumeration<Integer> relEnum = relationshipFrom.keys();
-		while (relEnum.hasMoreElements()) {
-			Integer relID = relEnum.nextElement();
-			String fromRef = relationshipFrom.get(relID);
-			String toPath = relationshipTo.get(relID);
-			String toRef = createRelatedNode(toPath, false);
-			addEdge(fromRef, toRef, true, true, "");
-		}
 		
 		progressFrame.dispose();
 		commonPart();	
@@ -213,6 +186,8 @@ public class TreeImport2 implements ActionListener {
 	public void fileTree(File file, String parentID, DefaultMutableTreeNode parentInTree,
 			int level) {
 	
+		int folderCounter = 0;
+		int leafCounter = 0;
 		File[] dirList = file.listFiles();
 		String content = "";
 		Long modDate = 0L;
@@ -233,6 +208,7 @@ public class TreeImport2 implements ActionListener {
 			String desti = "";
 
 			if (f.isDirectory()) {
+				folderCounter++;
 				if (!windows) {	// Maybe symlink
 					try {
 						desti = f.getCanonicalPath();
@@ -274,7 +250,7 @@ public class TreeImport2 implements ActionListener {
 				GraphNode node = nodes.get(nodeNum);
 				node.setDetail(moreDetail);
 
-				// add color and edge
+				// add hierarchy color and edge
 				String edgeColor = "";
 					nodeColors.put(nodeNum, colors[level % 6]);
 					edgeColor = colors2[level % 6];
@@ -282,6 +258,7 @@ public class TreeImport2 implements ActionListener {
 
 			} else { // not a directory
 				
+				leafCounter++;
 				modDate = f.lastModified();
 				if (modDate > maxModDate) maxModDate = modDate;
 				
@@ -314,6 +291,7 @@ public class TreeImport2 implements ActionListener {
 		
 		// details for parent
 		int parentNum = inputID2num.get(parentID);
+		content = "<br>" + folderCounter + " folders, " + leafCounter + " files<br>" + content;
 		nodeDetails.put(parentNum, content);
 		if (!file.isDirectory()) return;
 		if (maxModDate <= 0L) maxModDate = file.lastModified(); // only last resort
@@ -321,11 +299,21 @@ public class TreeImport2 implements ActionListener {
 		while (datesMap.containsKey(maxModDate)) maxModDate++;
 		datesMap.put(maxModDate, parentNum);
 		nodeDates.put(parentNum, maxModDate);
+		
+		double uniq = 0.;
+		uniq = ((double) leafCounter);
+		if (leafCounter > 20 && similarExtensions(file)) {
+			System.out.println("  " + file.getName() + " not marked as mess");
+			uniq = 1;
+		}
+		while (messMap.containsKey(uniq)) uniq += disambig;
+		messMap.put(uniq, parentNum);
+
 	}
 	
-	public String createRelatedNode(String desti, boolean siteMap) {
+	public String createRelatedNode(String desti) {
 		String fromRef = "";
-		if (byPaths.containsValue(desti) && !siteMap) {
+		if (byPaths.containsValue(desti)) {
 			// Find key
 			Enumeration<String> pathsEnum = byPaths.keys();
 			while (pathsEnum.hasMoreElements()) {
@@ -354,7 +342,7 @@ public class TreeImport2 implements ActionListener {
 			String detail = "<html><body>Open folder <a href=\"" + f.toURI().toString()  + "\">" + desti + "</a></body></html>";
 			addNode(destID, detail, desti != topNode);
 			
-			fromRef = createRelatedNode(ancestors, false);	// recurse
+			fromRef = createRelatedNode(ancestors);	// recurse
 			
 			addEdge(fromRef, destID, false, true, "");
 			if (!f.exists()) {
@@ -382,6 +370,14 @@ public class TreeImport2 implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.addWindowListener(myWindowAdapter);
 		frame.setLayout(new BorderLayout());
+		
+		Icon myLeaf = renderer.getOpenIcon();
+		UIManager.put("Tree.leafIcon", myLeaf);		// leaf entries here are folders, too
+		
+	    tree = new JTree(model);
+	    tree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+		frame.add(new JScrollPane(tree));
+		frame.setTitle("Found this tree structure:");
 
         JPanel toolbar = new JPanel();
         toolbar.setLayout(new BorderLayout());
@@ -414,11 +410,17 @@ public class TreeImport2 implements ActionListener {
 		layoutBox = new JCheckBox ("Tree layout", layoutOpt);
 		layoutBox.addActionListener(this);
 		optics.add(layoutBox);
-		colorBox = new JCheckBox ("Node color by change date", true);
+		colorBox = new JCheckBox ("Node color by change date", false);
 		colorBox.setActionCommand("colorDate");
 		colorBox.addActionListener(this);
 		optics.add(colorBox);
+		messBox = new JCheckBox ("Node color by mess", false);
+		messBox.setToolTipText("Many files outside of folders?");
+		messBox.setActionCommand("colorMess");
+		messBox.addActionListener(this);
+		optics.add(messBox);
 		legendBox = new JCheckBox ("Show legend", false);
+		legendBox.setEnabled(false);
 		legendBox.addActionListener(this);
 		optics.add(legendBox);
 		toolbar2.add(buttons,"East");
@@ -443,7 +445,75 @@ public class TreeImport2 implements ActionListener {
 //		Pass on the new map
 
 	public void finish() {
+		// Prepare coloring the folders by age 
+		// (by hierarchy is done via addEdge) 			TODO make more transparent
+
+		SortedSet<Long> datesSet = (SortedSet<Long>) datesList.keySet();
+		Iterator<Long> ixit = datesSet.iterator(); 
+		if (datesSet.size() > 0) {
+			int rangeSize = (datesSet.size() / 6) + 1;
+			int counter = datesSet.size() - 1;
+			int previousCol = -1;
+
+			while (ixit.hasNext()) {
+				int colID = counter / rangeSize;
+				counter--;
+				Long modDate = ixit.next();
+				int nodeNum = datesList.get(modDate);
+
+				nodeColors.put(nodeNum, colors[colID]);
+
+				//	Legend item
+				Date date = new Date(modDate);
+				SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+				String dateText = df2.format(date);
+				if (colID != previousCol) {
+					String label = dateText + " +";
+					if (colID == 0) label = label + " (newest)";
+					if (colID == 5) label = label + " (oldest)";
+					legend[colID] = label;
+					previousCol = colID;
+				}
+			}
+		}
+
+		// Prepare coloring the folders by mess 
+		if (messOpt) {
+	    SortedSet<Double> messSet = (SortedSet<Double>) messList.keySet();
+	    Iterator<Double> iter = messSet.iterator();
+	    int m = 0;
+	    while (iter.hasNext()) {
+	    	double pos = iter.next();
+	    	int nodeID = messMap.get(pos);
+	    	GraphNode messNode = nodes.get(nodeID);
+	    	m++;
+	    	if (m <= messMap.size() * .90) continue;
+	    	if (m > messMap.size() * .90) messNode.setColor("#ffff00");
+	    	if (m > messMap.size() * .95) messNode.setColor("#ffc800");
+	    	if (m > messMap.size() * .97) messNode.setColor("#ff0000");
+			Color color = messNode.getColor();
+			int r = color.getRed();
+			int g = color.getGreen();
+			int b = color.getBlue();
+			String colorString = String.format("#%02x%02x%02x", r, g, b);
+	    	colorPath(messNode, colorString);
+	    }
+		}
+
+	    if (!colorOpt && !messOpt ) {
+		//	Collect relationships
+		Enumeration<Integer> relEnum = relationshipFrom.keys();
+		while (relEnum.hasMoreElements()) {
+			Integer relID = relEnum.nextElement();
+			String fromRef = relationshipFrom.get(relID);
+			String toPath = relationshipTo.get(relID);
+			String toRef = createRelatedNode(toPath);
+			addEdge(fromRef, toRef, true, true, "");
+		}
+	    }
+	    
 		if (legendOpt) {
+			if (colorOpt) {
 			String legendID = readCount++ + "";
 			inputItems.put(legendID, "Legend");
 			addNode(legendID, "Look at the colored nodes connected to this one, "
@@ -462,6 +532,31 @@ public class TreeImport2 implements ActionListener {
 				int nodeNumL = inputID2num.get(id2);
 				nodeColors.put(nodeNumL, colors[colID]);
 			}
+			} else if (messOpt) {
+				String legendID = readCount++ + "";
+				inputItems.put(legendID, "Legend");
+				addNode(legendID, "Look at the colored nodes connected to this one, "
+						+ "<br />from red (worst) to yellow (modest). "
+						+ "<br />Mess is where many files are outside folders."
+						+ "<br /><br />Black icons are dead shortcuts (maybe just character variations)."
+						+ "<br />Grey icons are parents."
+						+ "<br /><br />(Folders with many files of the same 2 types "
+						+ "are not marked)", true);
+				String[] legend = {"top 3 %", "top 5 %", "top 10 %"};
+				for (int colID = 0; colID < 3; colID++) {
+					String id2 = readCount++ + "";
+					String label = legend[colID];
+					if (label == null) break;
+					inputItems.put(id2, label);
+					
+					addNode(id2, legend[colID] + " of the messiest folders are shown in this or a hotter color.", true);
+					int nodeNumL = inputID2num.get(id2);
+					GraphNode legendNode = nodes.get(nodeNumL);
+					legendNode.setColor(colors[colID]);
+					addEdge(legendID, id2, false, true, "");
+				}
+			}
+
 		}
 		if (transit) { 
 			Iterator<GraphEdge> xrefTreeIter = xrefTreeEdges.iterator();
@@ -482,7 +577,11 @@ public class TreeImport2 implements ActionListener {
 			if (layoutOpt) {
 
 				CentralityColoring centralityColoring = new CentralityColoring(nodes, edges);
+				centralityColoring.changeColors();
+				centralityColoring.revertColors();		// TODO reform constructor
 				centralityColoring.treeLayout(nonTreeEdges);
+
+				if (!messOpt) {
 				//	Recolor 
 				Enumeration<Integer> edgeColEnum = edgeColors.keys();
 				while (edgeColEnum.hasMoreElements()) {
@@ -498,6 +597,7 @@ public class TreeImport2 implements ActionListener {
 					GraphNode node = nodes.get(key);
 					node.setColor(treeColor);
 				}
+				
 				if (!colorOpt) {
 					//	Color nodes like edges end
 					Enumeration<Integer> edgesEnum = edges.keys();
@@ -513,7 +613,10 @@ public class TreeImport2 implements ActionListener {
 						}
 					}
 				}
+				}
+				
 			}
+			if (cancelled) return;
         	try {
         		dataString = new TopicMapStorer(nodes, edges).createTopicmapString();
         	} catch (TransformerConfigurationException e1) {
@@ -580,17 +683,60 @@ public class TreeImport2 implements ActionListener {
 		GraphNode targetNode = nodes.get(inputID2num.get(toRef));
 		edge = new GraphEdge(edgesNum, sourceNode, targetNode, Color.decode(newEdgeColor), "");
 		edges.put(edgesNum, edge);
+		sourceNode.addEdge(edge);
+		targetNode.addEdge(edge);
 		if (!treeColor.isEmpty()) edgeColors.put(edgesNum, treeColor);
 		if (xref) nonTreeEdges.add(edge);
 		if (removeBeforeReexport) xrefTreeEdges.add(edge);
+	}
+
+	// Accessories for mess detection
+	public boolean similarExtensions(File file) {
+		int leafCounter = 0;
+		HashSet<String> extensions = new HashSet<String>();
+		File[] dirList = file.listFiles();
+		if (dirList != null) {
+			for (File f : dirList) {
+				if (f.isHidden()) continue;
+				if (f.isDirectory()) {
+					continue;
+				} else {
+					leafCounter++;
+					int extOffset = f.getName().lastIndexOf(".");
+					String foundExtension = "";
+					if (extOffset > 0) foundExtension = f.getName().substring(extOffset);
+					extensions.add(foundExtension.toLowerCase());
+				}
+			}
+		}
+		if (extensions.size() <= 2) {
+			System.out.println(file.getName() + " " + leafCounter + ": " + extensions);
+			return true;
+		}
+		return false;
+	}
+	
+	public void colorPath(GraphNode node, String colorString) {
+		Enumeration<GraphEdge> neighbors = node.getEdges();
+		while (neighbors.hasMoreElements()) {
+			GraphEdge edge = neighbors.nextElement();
+			GraphNode dest = edge.getNode2();
+			if (!dest.equals(node)) continue;
+			GraphNode other = node.relatedNode(edge);
+			edge.setColor(colorString);
+			colorPath(other, colorString); 		// recursion
+		}
 	}
 	
 	public void actionPerformed(ActionEvent arg0) {
 		String command = arg0.getActionCommand();
 		if (command == "Cancel") {
 			transit = false;
+			cancelled = true;
+			System.out.println("Import cancelled");
 		} else if (command == "Continue") {
 			transit = transitBox.isSelected();
+			System.out.println("ColorOpt: " + colorOpt + ", MessOpt: " + messOpt);
 			
 		// Options interdependent
 		} else if (command == "transit"){
@@ -607,7 +753,15 @@ public class TreeImport2 implements ActionListener {
 			return;
 		} else if (command == "colorDate"){
 			colorOpt = colorBox.isSelected();
-			legendBox.setEnabled(colorOpt);
+			messOpt = !colorBox.isSelected();
+			messBox.setSelected(messOpt);
+			legendBox.setEnabled(colorOpt || messOpt);
+			return;
+		} else if (command == "colorMess"){
+			messOpt = messBox.isSelected();
+			colorOpt = !messBox.isSelected();
+			colorBox.setSelected(colorOpt);
+			legendBox.setEnabled(colorOpt || messOpt);
 			return;
 		} else if (command == "Show legend"){
 			legendOpt = legendBox.isSelected();
@@ -617,6 +771,8 @@ public class TreeImport2 implements ActionListener {
 			return;
 		}
         frame.setVisible(false);
+		Icon myLeaf = renderer.getLeafIcon();
+		UIManager.put("Tree.leafIcon", myLeaf);		// leaf entries here are folders, too
         frame.dispose();
         finish();
 		if (hypOpt) controler.getControlerExtras().toggleHyp(1, true);
